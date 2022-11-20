40600a0aa26af97d79d071cf7ad8bb5a7a9f9341
songhee24 <akaratel61@gmail.com>
2022-11-20 19:40:05 +0600

    creating first screen

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+///  Created by mac on 20/11/22.
+class ProductsOverviewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
