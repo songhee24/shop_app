a308b9e66bc0531cc880c5d37003dea1dc7f7974
songhee24 <akaratel61@gmail.com>
2022-11-20 21:51:31 +0600

    implementing fonts into project

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Lato',
         colorScheme: ColorScheme.fromSwatch(
           primarySwatch: Colors.purple,
         ).copyWith(
