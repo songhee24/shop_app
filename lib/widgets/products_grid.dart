287f37f643943b84b8802825acd7e778d2daa2b1
songhee24 <akaratel61@gmail.com>
2022-11-22 16:16:49 +0600

    fully accessing to our products by products' provider logic

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../models/Product.dart';
+import '../providers/products_provider.dart';
 import 'product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
   const ProductsGrid({
     Key? key,
-    required this.loadedProduct,
   }) : super(key: key);
-
-  final List<Product> loadedProduct;
 
   @override
   Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    final products = productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
-      itemCount: loadedProduct.length,
+      itemCount: products.length,
       itemBuilder: (ctx, i) => ProductItem(
-        id: loadedProduct[i].id,
-        title: loadedProduct[i].title,
-        imageUrl: loadedProduct[i].imageUrl,
+        id: products[i].id,
+        title: products[i].title,
+        imageUrl: products[i].imageUrl,
       ),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
