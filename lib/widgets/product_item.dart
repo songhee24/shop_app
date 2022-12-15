96e0551c57b0811c8d80ca4b9a30815ec21e7a68
songhee24 <akaratel61@gmail.com>
2022-12-15 19:58:15 +0600

    trying to use cart provider method

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/product_provider.dart';
 
+import '../providers/cart_provider.dart';
 import '../screens/product_detail_screen.dart';
 
 ///  Created by mac on 20/11/22.
@@ -14,6 +15,7 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     ProductProvider product =
         Provider.of<ProductProvider>(context, listen: false);
+    CartProvider cart = Provider.of<CartProvider>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -42,7 +44,13 @@ class ProductItem extends StatelessWidget {
               Icons.shopping_cart,
               color: Theme.of(context).colorScheme.secondary,
             ),
-            onPressed: () {},
+            onPressed: () {
+              cart.addItem(
+                product.id,
+                product.price as String,
+                product.title,
+              );
+            },
           ),
         ),
         child: GestureDetector(
