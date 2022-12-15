868e00b803879fb4bfda137bca125aed5c0430a2
songhee24 <akaratel61@gmail.com>
2022-12-15 18:01:14 +0600

    When you use provider of, then the whole build method will rerun whenever that data changes.
    but you could always have a case where you only want to run a subpart of your widgets tree when some
    
    data changes and then you could only wrap the subpart of the widget tree
    
    that depends on your product data with that listener.
    
    That would be an option
    
    and even here for example, you could get your product here with listen: false, to get all that one time data
    
    which you don't expect to change, like the title or the imageUrl and you could then use consumer,

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -12,46 +12,48 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ProductProvider product = Provider.of<ProductProvider>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        footer: GridTileBar(
-          backgroundColor: Colors.black54,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
-              color: Theme.of(context).colorScheme.secondary,
+    // ProductProvider product = Provider.of<ProductProvider>(context);
+    return Consumer<ProductProvider>(
+      builder: (context, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          footer: GridTileBar(
+            backgroundColor: Colors.black54,
+            leading: IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
             ),
-            onPressed: () {
-              product.toggleFavoriteStatus();
+            title: Text(
+              product.title,
+              style: const TextStyle(
+                fontSize: 12,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              onPressed: () {},
+            ),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: product.id,
+              );
             },
-          ),
-          title: Text(
-            product.title,
-            style: const TextStyle(
-              fontSize: 12,
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
             ),
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: Icon(
-              Icons.shopping_cart,
-              color: Theme.of(context).colorScheme.secondary,
-            ),
-            onPressed: () {},
-          ),
-        ),
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.id,
-            );
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
           ),
         ),
       ),
