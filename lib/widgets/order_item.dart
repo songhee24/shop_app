8f2f659551b4a8f7f3fc7a24fdec05aabda793c3
songhee24 <akaratel61@gmail.com>
2022-12-16 14:19:13 +0600

    order item created

---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../providers/orders_provider.dart' as oi;
+
+///  Created by mac on 16/12/22.
+class OrderItem extends StatelessWidget {
+  final oi.OrderItem orderItem;
+
+  OrderItem(this.orderItem);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('\$${orderItem.amount}'),
+          )
+        ],
+      ),
+    );
+  }
+}
