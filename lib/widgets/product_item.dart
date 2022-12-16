84babd97820f2bc94cb3d6d70fc0010018286e7d
songhee24 <akaratel61@gmail.com>
2022-12-16 16:05:43 +0600

    hideCurrentSnackBar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -50,6 +50,7 @@ class ProductItem extends StatelessWidget {
                 product.price,
                 product.title,
               );
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   duration: const Duration(
