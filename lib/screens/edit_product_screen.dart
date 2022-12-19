8ed1fbe83447cca69635a4e7d4e65c2dffcaf672
songhee24 <akaratel61@gmail.com>
2022-12-19 14:39:50 +0600

    new form widget

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+///  Created by mac on 19/12/22.
+class EditproductScreen extends StatefulWidget {
+  const EditproductScreen({super.key});
+
+  @override
+  State<EditproductScreen> createState() => _EditproductScreenState();
+}
+
+class _EditproductScreenState extends State<EditproductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
