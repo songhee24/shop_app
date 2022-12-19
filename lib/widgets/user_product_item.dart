8b21908a53389d5ed94ac0cece935ddd3d1967a5
songhee24 <akaratel61@gmail.com>
2022-12-19 11:43:11 +0600

    done with new separate widget

---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+///  Created by mac on 19/12/22.
+class UserProductItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  UserProductItem(this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing: Row(
+        children: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.edit),
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.delete),
+            color: Theme.of(context).errorColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
