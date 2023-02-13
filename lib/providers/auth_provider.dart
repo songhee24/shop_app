4ae3241f5c84d6e437899e91b241372475646092
songhee24 <akaratel61@gmail.com>
2023-02-14 00:11:46 +0600

    handling error

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_exception.dart';
 
 class AuthProvider with ChangeNotifier {
   late final String _token;
@@ -10,18 +11,26 @@ class AuthProvider with ChangeNotifier {
 
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
-    final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyB5I5btzUs9mIhNsuItFKv_q4dywTCFKlM');
-    final response = await http.post(
-      url,
-      body: jsonEncode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
+    try {
+      final url = Uri.parse(
+          'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyB5I5btzUs9mIhNsuItFKv_q4dywTCFKlM');
+      final response = await http.post(
+        url,
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }),
+      );
+      final responseData = jsonDecode(response.body);
+      print(responseData);
 
-    print(jsonDecode(response.body));
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
