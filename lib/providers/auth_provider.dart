e073116da3a8012d5bfb438f6c93fae5629915fc
songhee24 <akaratel61@gmail.com>
2023-02-13 23:33:48 +0600

    created auth provider

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,9 +1,23 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 class AuthProvider with ChangeNotifier {
   late final String _token;
   late final DateTime _expiryDate;
   late final String _userId;
 
-  Future<void> signup(String email, String password) async {}
+  Future<void> signup(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB5I5btzUs9mIhNsuItFKv_q4dywTCFKlM');
+    http.post(
+      url,
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+        'returnSecureToken': true,
+      }),
+    );
+  }
 }
